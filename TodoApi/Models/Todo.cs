57713882074a320fcb74277a2d8b54e02617ca9b
muratvuranok <muratvuranok@hotmail.com>
2024-01-06 17:42:19 +0400

    ProjectSetup

---
+++ TodoApi/Models/Todo.cs
@@ -0,0 +1,73 @@
+﻿using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace TodoApi.Models;
+public class Todo
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsCompleted { get; set; }
+    public string? Icon { get; set; }
+}
+
+
+public static class TodoEndpoints
+{
+	public static void MapTodoEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Todo").WithTags(nameof(Todo));
+
+        group.MapGet("/", async (ApplicationDbContext db) =>
+        {
+            return await db.Todos.ToListAsync();
+        })
+        .WithName("GetAllTodos")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Todo>, NotFound>> (int id, ApplicationDbContext db) =>
+        {
+            return await db.Todos.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Todo model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetTodoById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Todo todo, ApplicationDbContext db) =>
+        {
+            var affected = await db.Todos
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.Id, todo.Id)
+                  .SetProperty(m => m.Name, todo.Name)
+                  .SetProperty(m => m.IsCompleted, todo.IsCompleted)
+                  .SetProperty(m => m.Icon, todo.Icon)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateTodo")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Todo todo, ApplicationDbContext db) =>
+        {
+            db.Todos.Add(todo);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Todo/{todo.Id}",todo);
+        })
+        .WithName("CreateTodo")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, ApplicationDbContext db) =>
+        {
+            var affected = await db.Todos
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteTodo")
+        .WithOpenApi();
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+ 
+
+builder.Services.AddControllers(); 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapTodoEndpoints();
+
+app.Run();
